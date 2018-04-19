fc6d5271b48e6663ae6f4c2288741875601b4ca6
krishnacck <krishnacck@gmail.com>
2018-04-19 20:36:58 +0530

    updated app.py

--- app.py
+++ app.py
@@ -8,55 +8,80 @@ This file creates your application.
 
 import os
 from flask import Flask, render_template, request, redirect, url_for
+import requests
+import json
+import sys
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
 
-###
-# Routing for your application.
-###
+@app.route("/")
+def index():
+    return "Index!"
+ 
+@app.route("/hello")
+def hello():
+    return "Hello World!"
 
-@app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
+@app.route("/hello/<string:name>/")
+def test(name):
+    url = "http://blog.isochal.com/wp-json/wp/v2/posts/"
+    res = ""
+    r = requests.get(url)
+    po = r.json()
+    count = 0
+    for i in po:
+        # print(i)
+        # print("")
+        # print("")
+        # print("")
+        # print("")
+        # print("")
+        # print("")
+        # print(count)
+        result = {}
+        post = i
+        result['post_id'] = post['id']
+        post_id = post['id']
+        # print (post_id)
+        result['published_date'] = post['date']
+        result['status'] = post['status']
+        result['title'] = post['title']['rendered']
+        result['content'] = post['content']['rendered']
+        # print (content)
+        # sys.exit()
+        url = "http://blog.isochal.com/wp-json/wp/v2/categories?post="+str(post_id)
+        # print(url)
+        r = requests.get(url)
+        cate = r.json()
+        result['category'] = cate[0]['name']
+        url = "http://blog.isochal.com/wp-json/wp/v2/media?parent="+str(post_id)
+        r3 = requests.get(url)
+        media = r3.json()
+        result['image'] = media[0]['guid']['rendered']
+        # result = result.append()
+        # res = res.append(json.dumps(result))
+        # print(result)
+        count = count+1
+        if len(res)>1:
+            res = res, result
+        else:
+            res = result
+        # print (res)
+    # res = res, result      
+    # print(res) 
+    # print (res)
+    return json.dumps(res)
 
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/members")
+def members():
+    return "Members"
+ 
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name
+ 
+if __name__ == "__main__":
+    app.run()
+    # test(1711)
