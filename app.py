e976d2e1e1724e44660251237b41416ef9efe021
krishnacck <krishnacck@gmail.com>
2018-04-19 20:45:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,59 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-
-This file creates your application.
-"""
-
-import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 import requests
 import json
+import urllib
 import sys
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
-
-
+ 
 @app.route("/")
 def index():
-    return "Index!"
+    url = "http://blog.isochal.com/wp-json/wp/v2/posts/"
+    res = ""
+    r = requests.get(url)
+    po = r.json()
+    count = 0
+    for i in po:
+        # print(i)
+        # print("")
+        # print("")
+        # print("")
+        # print("")
+        # print("")
+        # print("")
+        # print(count)
+        result = {}
+        post = i
+        result['post_id'] = post['id']
+        post_id = post['id']
+        # print (post_id)
+        result['published_date'] = post['date']
+        result['status'] = post['status']
+        result['title'] = post['title']['rendered']
+        result['content'] = post['content']['rendered']
+        # print (content)
+        # sys.exit()
+        url = "http://blog.isochal.com/wp-json/wp/v2/categories?post="+str(post_id)
+        # print(url)
+        r = requests.get(url)
+        cate = r.json()
+        result['category'] = cate[0]['name']
+        url = "http://blog.isochal.com/wp-json/wp/v2/media?parent="+str(post_id)
+        r3 = requests.get(url)
+        media = r3.json()
+        result['image'] = media[0]['guid']['rendered']
+        # result = result.append()
+        # res = res.append(json.dumps(result))
+        # print(result)
+        count = count+1
+        if len(res)>1:
+            res = res, result
+        else:
+            res = result
+        # print (res)
+    # res = res, result      
+    # print(res) 
+    # print (res)
+    return json.dumps(res)
  
 @app.route("/hello")
 def hello():
@@ -83,5 +118,5 @@ def getMember(name):
     return name
  
 if __name__ == "__main__":
-    app.run()(debug=True)
+    app.run()
     # test(1711)
